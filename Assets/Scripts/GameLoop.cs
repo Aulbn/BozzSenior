add358e525cd4783392eac78c638c8d960c35d1a
Albin <38533964+Aulbn@users.noreply.github.com>
2021-01-23 15:19:29 +0100

    GameLoop init

---
+++ Assets/Scripts/GameLoop.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoop : MonoBehaviour
+{
+    //List of paralell and sequencial items, including actions, waiting timers and conditional pausers.
+    //Also count for how many times a loop will loop
+
+
+}
